16461ceba4561eba9be327794f906ba3b7f0f9ed
Silvano Stralla <silvano.stralla@sistrall.it>
2024-05-28 09:50:38 +0200

    Let Next.js parse the POST body

--- pages/api/invalidate-pages.ts
+++ pages/api/invalidate-pages.ts
@@ -8,24 +8,25 @@ type Data = {
 };
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
-  if (req.method === 'POST') {
-    if (req.headers['Webhook-Token'] !== process.env.WEBHOOK_TOKEN) {
-      res.status(401);
+  try {
+    if (req.method === 'POST') {
+      if (req.headers['webhook-token'] !== process.env.WEBHOOK_TOKEN) {
+        res.status(401);
+      }
+      else {
+        const data = req.body;
+      
+        const cacheTags = data['entity']['attributes']['tags'].map((tag: string) => tag as CacheTag);
+      
+        await invalidateCacheTags(cacheTags);
+      
+        res.status(200).json({ cacheTags });
+      }
     }
     else {
-      // Read the req content: that's a comma separated list of cache tags sent
-      // by DatoCMS as the body of the webhook.
-      const body = req.body;
-    
-      const data = JSON.parse(body);
-    
-      const cacheTags = data['entity']['attributes']['tags'].map((tag: string) => tag as CacheTag);
-    
-      await invalidateCacheTags(cacheTags);
-    
-      res.status(200).json({ cacheTags });
+      res.status(200).json({ cacheTags: [] });
     }
+  } catch (err) {
+    res.status(500).send({ error: err.message })
   }
-
-  res.status(200).json({ cacheTags: [] });
 }
